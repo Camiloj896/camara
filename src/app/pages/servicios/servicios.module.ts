9d2ada69d84900c2063173f3a4a91e5771b1d604
Cristian Niño <cristian.david@soluciones.software>
2019-08-28 15:09:55 -0500

    first commit

---
+++ src/app/pages/servicios/servicios.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ServiciosPage } from './servicios.page';
+
+import { HeaderComponent } from './../../components/header/header.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ServiciosPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [ServiciosPage, HeaderComponent]
+})
+export class ServiciosPageModule {}
