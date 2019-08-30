4e1391f89f0c8b1b8e4518c448ef5a8a83745adb
Cristian Niño <cristian.david@soluciones.software>
2019-08-30 16:32:54 -0500

    Version 1

--- src/app/pages/transacciones/transacciones.page.ts
+++ src/app/pages/transacciones/transacciones.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InAppBrowser, InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-transacciones',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransaccionesPage implements OnInit {
 
-  constructor() { }
+  constructor(private browser : InAppBrowser) { }
 
   ngOnInit() {
+  }
+
+  openUrl(){
+    let options:InAppBrowserOptions = {
+      hidden:"yes",
+      hidenavigationbuttons:"yes",
+      hideurlbar: "yes",
+      hardwareback: "yes",
+      footer: "yes",
+      footercolor: "blue"
+    }
+    this.browser.create("https://www.ccb.org.co" , "_self", options)
   }
 
 }
